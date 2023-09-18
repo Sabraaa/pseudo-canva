e2ff4b36780f9e402f1f598ddad5fc909256bb3d
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-09-18 20:50:41 +0330

    imma head out

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../src/assets/logo.svg";
+import Profile from "../src/assets/avatar.jpg"
 const Header = () => {
   return (
-    <div className='bg-red-700'>Header</div>
-  )
-}
+    <div>
+      <div className="container">
+        <div className="left-side display: flex;">
+          <button></button>
+          <img srs={Profile} alt="profile" />
+          <button></button>
+        </div>
+        <div className="right-side">
+          <input type="text" />
+          <div className="icons"></div>
+          <button></button>
+          <img src={Logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
