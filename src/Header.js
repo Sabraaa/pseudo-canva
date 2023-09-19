ff95ba03cc58dc27e4f676010b3c4a442582abb4
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-09-19 20:58:29 +0330

    headered

--- src/Header.js
+++ src/Header.js
@@ -13,50 +13,59 @@ import {
 const Header = () => {
   return (
     <div>
-      <div className="header-container flex justify-between w-full border-b-2">
+      <div className="header-container flex justify-between w-full border-b-2 h-20">
         {/* left side of header */}
         <div className="header-left-side flex justify-start p-6 items-center">
-          <RiMenuFill color="000" size={25} className="cursor-pointer" />
-          <img className="px-6 cursor-pointer" srs={Logo} alt="logo" />
+          <div className="cursor-pointer  hover:bg-slate-200 rounded p-2">
+            <RiMenuFill color="000" size={20} />
+          </div>
+          <img className="px-6 cursor-pointer  " src={Logo} alt="logo" />
           {/* the dropdowns */}
-          <div className="dropdown-container flex px-5 cursor-pointer">
-            <p className="pl-5 ">Design spotlight</p>
-            <RiArrowDropDownLine color="000" size={25} />
-            <p className="pl-5">Business</p>
-            <RiArrowDropDownLine color="000" size={25} />
-            <p className="pl-5">Education</p>
-            <RiArrowDropDownLine color="000" size={25} />
-            <p className="pl-5">Plans and pricing</p>
-            <RiArrowDropDownLine color="000" size={25} />
-            <p className="pl-5">Learn</p>
-            <RiArrowDropDownLine color="000" size={25} />
+          <div className="dropdown-container flex px-5 cursor-pointer text-base">
+            <div className="cursor-pointer flex items-center hover:bg-slate-200 rounded p-2">
+              <p>Design spotlight</p>
+              <RiArrowDropDownLine color="000" size={25} />
+            </div>
+            <div className="cursor-pointer flex items-center hover:bg-slate-200 rounded p-2">
+              <p>Business</p>
+              <RiArrowDropDownLine color="000" size={25} />
+            </div>
+            <div className="cursor-pointer flex items-center hover:bg-slate-200 rounded p-2">
+              <p>Education</p>
+              <RiArrowDropDownLine color="000" size={25} />
+            </div>
+            <div className="cursor-pointer flex items-center hover:bg-slate-200 rounded p-2">
+              <p>Plans and pricing</p>
+              <RiArrowDropDownLine color="000" size={25} />
+            </div>
+            <div className="cursor-pointer flex items-center hover:bg-slate-200 rounded p-2">
+              <p>Learn</p>
+              <RiArrowDropDownLine color="000" size={25} />
+            </div>
           </div>
         </div>
 
         {/* the right side of header */}
         <div className="header-right-side flex justify-end p-6 items-center">
           <div className="icons flex ">
-            <RiComputerLine
-              className="cursor-pointer mr-3"
-              color="000"
-              size={25}
-            />
-            <RiSettings2Line
-              className="cursor-pointer mr-3"
-              color="000"
-              size={25}
-            />
-            <RiNotification2Line
-              className="cursor-pointer mr-3"
-              color="000"
-              size={25}
-            />
+            <div className="cursor-pointer p-3  hover:bg-slate-200 rounded">
+              <RiComputerLine color="000" size={22} />
+            </div>
+            <div className="cursor-pointer p-3  hover:bg-slate-200 rounded">
+              <RiSettings2Line color="000" size={22} />
+            </div>
+            <div className="cursor-pointer p-3  hover:bg-slate-200 rounded">
+              <RiNotification2Line color="000" size={22} />
+            </div>
           </div>
-          <button className="w-24 h-9 rounded text-sm px-3" type="button">
+          <button
+            className="w-22 h-9 rounded text-sm px-3 bg-violet-400 text-white mx-4"
+            type="button"
+          >
             Create a design
           </button>
           <img
-            className="  rounded-full w-10 h-10"
+            className=" cursor-pointer rounded-full w-10 h-10"
             src={Profile}
             alt="profile"
           />
