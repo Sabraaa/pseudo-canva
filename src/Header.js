6d46b1344c1f8a4d158466c77631051bd8cca1e8
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-09-19 21:24:54 +0330

    dropdown's left

--- src/Header.js
+++ src/Header.js
@@ -44,6 +44,26 @@ const Header = () => {
             <div className="cursor-pointer flex items-center hover:bg-slate-200 rounded p-2">
               <p>Design spotlight</p>
               <RiArrowDropDownLine color="000" size={25} />
+              <div id="dropdownInformation" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+    <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+      <div>Bonnie Green</div>
+      <div class="font-medium truncate">name@flowbite.com</div>
+    </div>
+    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+      </li>
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+      </li>
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+      </li>
+    </ul>
+    <div class="py-2">
+      <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+    </div>
+</div>
             </div>
             <div className="cursor-pointer flex items-center hover:bg-slate-200 rounded p-2">
               <p>Business</p>
