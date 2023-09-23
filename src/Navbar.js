e5abee5d07e582e4167b5e5d360d282936b22f30
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-09-23 20:02:55 +0330

    navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <h1>What will you design today?</h1>
+      <input />
+      {/* icons */}
+      <div></div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
